2e7fbefc945e00c73441da5426e0dc86423d12d3
Sharif Rashed <sharif.rashed26@gmail.com>
2022-04-29 11:45:01 -0500

    made Comic Book Controller index method

---
+++ InfinityComics1/Controllers/ComicBookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfinityComics1.Controllers
+{
+    [Authorize]
+    public class ComicBookController : Controller
+    {
+
+        private readonly IComicBookRepository _comicBookRepository;
+        public IActionResult Index()
+        {
+            var comicBook = _comicBookRepository.GetAllComicBooks();
+            return View(comicBooks);
+        }
+    }
+}
