3d684786c21ea15bfb6e5030024bda6e6b23b7d3
Sharif Rashed <sharif.rashed26@gmail.com>
2022-04-29 14:47:31 -0500

    finished repositories and fixed bugs

--- InfinityComics1/Controllers/ComicBookController.cs
+++ InfinityComics1/Controllers/ComicBookController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using InfinityComics1.Auth.Models;
+using InfinityComics1.Models;
+using InfinityComics1.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InfinityComics1.Controllers
 {
-    [Authorize]
     public class ComicBookController : Controller
     {
 
         private readonly IComicBookRepository _comicBookRepository;
+        public ComicBookController(IComicBookRepository comicBookRepository)
+        {
+            _comicBookRepository = comicBookRepository;
+        }
         public IActionResult Index()
         {
-            var comicBook = _comicBookRepository.GetAllComicBooks();
+            List<ComicBook> comicBooks = _comicBookRepository.GetAllComicBooks();
             return View(comicBooks);
         }
     }
---
+++ InfinityComics1/Repositories/IComicBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using InfinityComics1.Auth.Models;
+using InfinityComics1.Models;
+
+namespace InfinityComics1.Repositories
+{
+    public interface IComicBookRepository
+    {
+        void AddComicBook(ComicBook comicBook);
+        List<ComicBook> GetAllComicBooks();
+    }
+}
